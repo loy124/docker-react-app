aa3439637a2501305ab9cf7fca51f14ce5d158cd
onyou.lee <onyou.lee@mincoding.co.kr>
2020-12-29 10:04:44 +0900

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/반갑습니다.1/i);
   expect(linkElement).toBeInTheDocument();
 });
+
